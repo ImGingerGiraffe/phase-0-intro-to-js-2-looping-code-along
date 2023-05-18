24ad6b0c028814ff220387df604e4a06edd7feca
Caleb Bushard <bloodyybass@gmail.com>
2023-05-18 19:32:41 -0400

    looping lab

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solutions in this file
+const thankYouCards=[];
+    function writeCards(names, event) {
+        for (let cards = 0; cards < names.length; cards++) {
+           thankYouCards.push(`Thank you, ${names[cards]}, for the wonderful ${event} gift!`)
+        }
+        return thankYouCards
+    }
+
+    function countDown(integer) {
+        while (integer > -1) {
+            console.log(integer--)
+        }
+    }
